a2a682e9e07e7d2e083e6b8e02dbc1a2a09ae74d
Nikita Dmitriev <a2143319@yandex.ru>
2021-10-04 03:52:45 +0500

    Initial Commit

---
+++ TestOnlim/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestOnlim
+//
+//  Created by Никита Дмитриев on 04.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
